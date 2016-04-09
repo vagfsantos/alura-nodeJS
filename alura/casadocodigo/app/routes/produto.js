6b129c8db633fbd01afcbfad0553fbb0a1ed5408
Vagner Santos <santos.vagnerferreira@gmail.com>
2016-04-09 12:13:49 -0300

    connectionFactory

--- alura/casadocodigo/app/routes/produto.js
+++ alura/casadocodigo/app/routes/produto.js
@@ -1,18 +1,15 @@
+var connectionFactory = require('../infra/connectionFactory');
+
 module.exports = function(app){
 	app.get('/produtos', function(req, res){
 		var mysql = require('mysql');
-		var connection = mysql.createConnection({
-			'host' : 'localhost',
-			'user' : 'root',
-			'password' : '',
-			'database' : 'casadocodigo_nodejs'
-		});
-
+		var connection = connectionFactory();
+		
 		connection.query('select * from livros', function(error, results){
 			console.log(error);
-			//res.send(results);
 			res.render('produtos/lista', {'lista':results});
 		});
+		
 		connection.end();
 	});
 	console.log('exportei routes');
