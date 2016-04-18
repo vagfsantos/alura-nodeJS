68306f516fdbb8bb1a27db4f26975fa7d2407b3c
Vagner Santos <santos.vagnerferreira@gmail.com>
2016-04-17 23:36:13 -0300

    http verb post

--- alura/casadocodigo/app/routes/produto.js
+++ alura/casadocodigo/app/routes/produto.js
@@ -1,16 +1,31 @@
 module.exports = function(app){
 	app.get('/produtos', function(req, res){
-		var mysql = require('mysql');
 		var connection = app.infra.connectionFactory();
-		
-		var produtosBanco = new app.infra.ProdutosDAO(connection);
-		produtosBanco.lista(function(error, results){
+		var produtosDAO = new app.infra.ProdutosDAO(connection);
+
+		produtosDAO.lista(function(error, results){
 			console.log(error);
 			res.render('produtos/lista', {'lista':results});
 		});
 		
 		connection.end();
 	});
+
+	app.get('/produtos/form', function(req, res){
+		res.render('produtos/form');
+	});
+
+	app.post('/produtos', function(req, res){
+		var produto = req.body;
+		console.log(produto);
+
+		var connection = app.infra.connectionFactory();
+		var produtosDAO = new app.infra.ProdutosDAO(connection);
+		produtosDAO.salva(produto, function(error, results){
+			res.redirect('/produtos');
+		});
+	});
+
 	console.log('exportei routes');
 }
 
