a6829bc5ba0929b1d518dd0ec96044c84fd97cb0
Vagner Santos <santos.vagnerferreira@gmail.com>
2016-04-22 15:10:45 -0300

    tdd with surpertest

--- alura/casadocodigo/test/produtos.js
+++ alura/casadocodigo/test/produtos.js
@@ -1,28 +1,33 @@
-var http = require('http');
+var express = require('../config/express')();
+var request = require('supertest')(express);
 
 describe('#Produtos Controller', function(){
 
 	it('#listagem JSON', function(done){
-		var config = {
-			hostname:'localhost',
-			port:3000,
-			path:'/produtos',
-			headers:{
-				'Accept':'application/json'
-			}
-		};
+		request.get('/produtos')
+		.set('Accept', 'application/json')
+		.expect('Content-Type', /json/)
+		.expect(200, done);
+	});
 
-		http.get(config, function(res){
-			if(res.statusCode == 200){
-				console.log("Status OK");
-			}
-			if(res.headers['content-type'] == 'application/json'){
-				console.log("JSON received");
-			}
 
-			done();
-		});
+	it('#Cadastro de produtos com dados INVALIDOS', function(done){
+		request.post('/produtos')
+		.send({
+			titulo:'',
+			descricao:'Novo Livro'
+		})
+		.expect(400, done);
+	});
 
+	it('#Cadastro de produtos com dados VALIDOS', function(done){
+		request.post('/produtos')
+		.send({
+			titulo:'Livro de teste',
+			descricao:'Novo Livro de teste',
+			preco:'16.5'
+		})
+		.expect(302, done);
 	});
 
 });
