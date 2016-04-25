a1d21c3f4de3af30b289d9207d74c71a4bfb7461
Vagner Santos <santos.vagnerferreira@gmail.com>
2016-04-24 21:42:17 -0300

    manual middlewares

--- alura/casadocodigo/config/express.js
+++ alura/casadocodigo/config/express.js
@@ -13,6 +13,20 @@ module.exports = function(){
 	app.use(expressValidator());
 
 	load('routes', {cwd:'app'}).then('infra').into(app);
+
+	app.use(function(req, res, next){
+		res.status(404).render('erros/404');
+		next();
+	});
+
+	app.use(function(error, req, res, next){
+		if(!process.env.NODE_ENV){
+			res.status(500).render('erros/500');
+			return;
+		}
+		next(error);
+	});
+
 	return app;
 }
 
